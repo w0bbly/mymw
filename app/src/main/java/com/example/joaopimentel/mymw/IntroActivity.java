796f693af133fedccc24ea26d2c2d6ad939d113a
JoaoPimentel <joao.pimentel@readinessit.com>
2018-02-21 16:26:33 +0000

    Commit #3 - Intro Animation done

--- app/src/main/java/com/example/joaopimentel/mymw/IntroActivity.java
+++ app/src/main/java/com/example/joaopimentel/mymw/IntroActivity.java
@@ -3,22 +3,29 @@ package com.example.joaopimentel.mymw;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Window;
-import android.view.WindowManager;
+import android.view.animation.AnimationUtils;
 import android.widget.TextView;
-
+import android.view.animation.Animation;
 public class IntroActivity extends AppCompatActivity {
 
     TextView appManage;
     TextView appYour;
     TextView appMoney;
     TextView appWisely;
+    TextView creatorName;
+    Animation fadeInAnimation;
+
+    Animation fadeInAnimation() {
+        fadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        return fadeInAnimation;
+    }
 
     void changeFontsOfText() {
         appManage = findViewById(R.id.app_manage);
         appMoney = findViewById(R.id.app_money);
         appYour = findViewById(R.id.app_your);
         appWisely = findViewById(R.id.app_wisely);
+        creatorName = findViewById(R.id.creator_name);
 
         Typeface custom_font = Typeface.createFromAsset(getAssets(),"fonts/alphabetized_cassette_tapes.ttf");
 
@@ -26,13 +33,18 @@ public class IntroActivity extends AppCompatActivity {
         appYour.setTypeface(custom_font);
         appMoney.setTypeface(custom_font);
         appWisely.setTypeface(custom_font);
+        creatorName.setTypeface(custom_font);
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
         changeFontsOfText();
+        appManage.startAnimation(fadeInAnimation());
+        appYour.startAnimation(fadeInAnimation());
+        appMoney.startAnimation(fadeInAnimation());
+        appWisely.startAnimation(fadeInAnimation());
     }
 
     @Override
